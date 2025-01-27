553e68599b996bdcb7dd14eadecda44cc893f12f
Sameer Pantvaidya <sameer.pantvaidya@cons.amplifon.com>
2025-01-27 17:29:35 +0530

    First Commit

---
+++ Calculations.Tests/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd_ReturnsInt()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Assert.Equal(3,result);
+        }
+
+        [Fact]
+        public void TestAddDouble_ReturnsDouble()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.AddDouble(1.4, 2.8);
+            Assert.Equal(4.2, result,1);
+        }
+    }
+}
---
+++ Calculations/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculations
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public double AddDouble(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
