758138cd7e557066ad6a866243c6e8a756acc286
Sameer Pantvaidya <sameer.pantvaidya@cons.amplifon.com>
2025-01-28 15:39:22 +0530

    ObjectTypes

--- UnitTest.Tests/CustomerTest.cs
+++ UnitTest.Tests/CustomerTest.cs
@@ -10,8 +10,16 @@ namespace UnitTest.Tests
         {
             var customer = new Customer();
             var exceptiondetails = Assert.Throws<ArgumentException>(
-                () => customer.GetCusotmerByName(""));
+                () => customer.GetCustomerByName(""));
             Assert.Equal("Hello", exceptiondetails.Message);
+        }
+
+        [Fact]
+        public void LoyalCustomerForOrderGreater100()
+        {
+            var customer = CustomerFactory.CreateCustomerInstance(101);
+            var loyalCustomer = Assert.IsType<LoyalCustomer>(customer);
+            Assert.Equal(10, loyalCustomer.Discount);
         }
     }
 }
