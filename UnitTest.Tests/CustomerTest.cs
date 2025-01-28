ff65f4477279f92274832b8382e845f691482bcc
Sameer Pantvaidya <sameer.pantvaidya@cons.amplifon.com>
2025-01-28 14:27:27 +0530

    UnitTests

---
+++ UnitTest.Tests/CustomerTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace UnitTest.Tests
+{
+    public class CustomerTest
+    {
+        //Asserting Exception
+        [Fact]
+        public void GetCusotmerByNameNotNull()
+        {
+            var customer = new Customer();
+            var exceptiondetails = Assert.Throws<ArgumentException>(
+                () => customer.GetCusotmerByName(""));
+            Assert.Equal("Hello", exceptiondetails.Message);
+        }
+    }
+}
